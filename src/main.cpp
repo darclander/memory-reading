8096437afef038a74453ae7b6f48a1d1373294df
Darclander <lukedarclander@gmail.com>
2020-05-13 16:11:49 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -67,17 +67,6 @@ uintptr_t GetModuleSize(DWORD procId, const wchar_t* modName) {
     return modBaseSize;
 }
 
-
-uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets) {
-    uintptr_t addr = ptr;
-    for (unsigned int i = 0; i < offsets.size(); ++i) {
-        ReadProcessMemory(hProc, (BYTE*)addr, &addr, sizeof(addr), 0);
-        addr += offsets[i];
-    }
-    return addr;
-}
-
-
 int main() {
 
     DWORD procId = GetProcId(L"x.exe");
@@ -89,21 +78,18 @@ int main() {
 
     uintptr_t dynamicPtrBaseAddr = moduleBase;
     uintptr_t dynamicPtrLastAddr = 2*(moduleBase + moduleSize);
-
     std::cout << "Base: " << dynamicPtrBaseAddr << std::endl;
     std::cout << "Last: " << dynamicPtrLastAddr << std::endl;
 
+
+    // Initializes values
     int number = 502;
     std::cout << &number;
+
     int value = 0;
-    int arr [100000] = {};
-    uintptr_t addresses[100000] = {};
-    int k=0;
-    int search = 0;
-    std::cin >> search;
-    DWORD oldProtect;
     std::ofstream myfile;
-    myfile.open("values2.txt");
+    myfile.open("values.txt"); // textfile to write to
+
     if (myfile.is_open()) {
         for (uintptr_t i = dynamicPtrBaseAddr; dynamicPtrBaseAddr <= dynamicPtrLastAddr; i++) {
             dynamicPtrBaseAddr += 4;
@@ -111,42 +97,8 @@ int main() {
             if(value > 0 && value < 1000) {
                 myfile << dynamicPtrBaseAddr << ": " << value << "\n";
             }
-            // if (value == search) {
-            //     VirtualProtectEx(hProcess, (LPVOID)dynamicPtrBaseAddr, sizeof(value), PAGE_READWRITE, &oldProtect);
-            //     value = 3000;
-            //     if(WriteProcessMemory(hProcess, (BYTE*)dynamicPtrBaseAddr, &value, sizeof(value),0) == 0) {
-            //         std::cout << GetLastError();
-            //         system("pause");
-            //     } 
-            //     arr[k] = value;
-            //     addresses[k] = dynamicPtrBaseAddr;
-            //     std::cout << k << std::endl;
-            //     std::cout << dynamicPtrBaseAddr << std::endl;
-            //     std::cout << arr[k] << std::endl;
-            //     k++;
-            // }
         }
     }
     myfile.close();
-    std::cout << dynamicPtrBaseAddr << std::endl;
-    std::cout << k;
-    std::cout << "hello" << std::endl;
     system("pause");
-    for (int i = 0; i <= 10; i++) {
-        std::cout << addresses[i] << std::endl;
-    }
-
-    
-    // system("pause");
-    // for (int i =0; i <= 10; i++) {
-    // ReadProcessMemory(hProcess, (BYTE*)addresses[i], &value, sizeof(value),0);
-    //     std::cout << addresses[i] << std::endl;
-    //     std::cout << value << std::endl;
-    // }
-    // system("pause");
 }
-
-
-    // std::vector<unsigned int> xOffsets = {0};
-    // uintptr_t xaddr = FindDMAAddy(hProcess, dynamicPtrBaseAddr, xOffsets);
-    // std::cout << "xaddr: " << "0x" << std::hex << xaddr << std::endl;
